4b8734d75686add963825deafb91096bad040a26
ConeDll <149405095+ConeDll@users.noreply.github.com>
2024-05-03 22:50:44 +0300

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,66 @@
+import socket
+import colorama
+from colorama import Fore
+import threading
+import os
+import json
+try:
+    with open("settings.json", "r") as check:
+        testoku = json.load(check)
+except FileNotFoundError:
+    testoku = {
+        "port": 1024,
+        "show-closed-ports": False,
+        "save":False
+    }
+    with open("settings.json", "w") as check:
+        json.dump(testoku, check, indent=4)
+if os.name == 'posix':
+    os.system('clear')
+elif os.name == 'nt':
+    os.system('cls')
+with open("settings.json","r") as file:
+    options = json.load(file)
+    portaraligi = options["port"]
+    save = options["save"]
+    showclosed = options["show-closed-ports"]
+    if showclosed == False:
+        gosterme = True
+    elif showclosed == True:
+        gosterme = False
+colorama.init()
+saveports = []
+def scan_port(target, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.1)
+        s.connect((target, port))
+        service = socket.getservbyport(port)
+        print(Fore.GREEN + f"{port} -> OPEN ({service})")
+        saveports.append(port)
+    except:
+        if gosterme == False:
+            print(Fore.RED + f"{port} -> CLOSED")
+        elif gosterme == True:
+            pass
+    finally:
+        s.close()
+def main():
+    target = input(f"{Fore.YELLOW}Target: ")
+    print(f"{Fore.CYAN}--------------------Starting Scan---------------------------")
+    start_port = 1
+    end_port = portaraligi
+    threads = []
+    
+    for port in range(start_port, end_port+1):
+        thread = threading.Thread(target=scan_port, args=(target, port))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+    print(f"{Fore.CYAN}[+]Completed")
+    if save == True:
+        with open(f"{target}_scan.txt","w") as kaydet:
+            kaydet.write(str(saveports))
+if __name__ == "__main__":
+    main()
